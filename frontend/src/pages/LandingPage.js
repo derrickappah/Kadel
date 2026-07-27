a8d529977654d2d480858f84be54fa06c2d31203
derrickappah <derrickappah17@gmail.com>
2026-07-27 16:12:26 +0000

    feat: update hero background slideshow to keep last 3 photos and add new photo

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -11,15 +11,10 @@ import { toast } from "sonner";
 const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
 
 const HERO_IMAGES = [
-  "/gallery/gallery-1.jpg",
-  "/gallery/gallery-2.jpg",
-  "/gallery/gallery-3.jpg",
-  "/gallery/gallery-4.jpg",
-  "/gallery/gallery-5.jpg",
-  "/gallery/gallery-6.jpg",
   "/gallery/gallery-7.jpg",
   "/gallery/gallery-8.jpg",
-  "/gallery/gallery-9.jpg"
+  "/gallery/gallery-9.jpg",
+  "/gallery/gallery-10.jpg"
 ];
 
 const steps = [
